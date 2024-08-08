cac84e2152303c48667090d20129d241c898668c
s0if <saifalkomi@gmail.com>
2024-08-08 23:22:36 +0300

    create product

---
+++ Context/ProductContext.cs
@@ -0,0 +1,19 @@
+﻿using create_product.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace create_product.Context
+{
+    internal class ProductContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=Saifaldin\\SQLEXPRESS02;Database=CreateProduct;Trusted_Connection=true;TrustServerCertificate=True");
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace create_product
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
